24229b0c2c23e71bce5e74f2285a61a714bba7ce
hnhan <hnhan@redhat.com>
2022-02-03 20:57:47 -0500

    add auto core pinnning for common threads and individual test 'setcore' (#6)

---
+++ log.py
@@ -0,0 +1,6 @@
+import logging
+logger = logging
+# DEBUG, INFO, WARNING
+logger.basicConfig(level=logging.INFO,
+    format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S')
