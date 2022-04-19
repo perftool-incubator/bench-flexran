c807459d836a105c89acaf49375d03e31804c480
HughNhan <hnhan@redhat.com>
2022-04-19 10:20:43 -0400

    merge xran back to flexran and remove PIC files

--- autotest.py
+++ autotest.py
@@ -15,12 +15,47 @@ from pprint import pprint
 import subprocess
 
 class Setting:
-    @classmethod
-    def update_cfg_files(cls, cfg: str, cpursc: CpuResource):
-        
-        l1_cfg_file = os.getenv('L1_CFG_FILE')
-        testmac_cfg_file = os.getenv('TESTMAC_CFG_FILE')
 
+    @classmethod
+    def update_xran_cfg_files(cls, oru_cfgfile: str, vfs_pci, cpursc: CpuResource):
+        print ("Updating XRAN %s:" % oru_cfgfile);
+        # Step 1: fix-up DU 2 VF PCIs
+        #<PciBusAddoRu0Vf0>0000:1a:02.0</PciBusAddoRu0Vf0>
+        #<PciBusAddoRu0Vf1>0000:1a:02.1</PciBusAddoRu0Vf1>
+        pci_addr=vfs_pci.split(",")
+
+        spec= "s#<PciBusAddoRu0Vf0>.*</PciBusAddoRu0Vf0>#<PciBusAddoRu0Vf0>PCI_ADDR</PciBusAddoRu0Vf0>#"  
+        result = re.sub("PCI_ADDR", str(pci_addr[0]), spec)
+        subprocess.call(["sed", "-i", result, oru_cfgfile])
+
+        spec= "s#<PciBusAddoRu0Vf1>.*</PciBusAddoRu0Vf1>#<PciBusAddoRu0Vf1>PCI_ADDR</PciBusAddoRu0Vf1>#"  
+        result = re.sub("PCI_ADDR", str(pci_addr[1]), spec)
+        subprocess.call(["sed", "-i", result, oru_cfgfile])
+
+        # Step 2: fix-up RU 2 VF MACs
+        # Crucible 09-ru_sriov.sh hardcode the below MACs. So no need to change
+        # <oRuRem0Mac0>00:11:22:33:00:01</oRuRem0Mac0>
+        # <oRuRem0Mac1>00:11:22:33:00:11</oRuRem0Mac1>
+
+        # step 3: Alloc a CPU for xRANThread
+        # <xRANThread>18, 96, 0</xRANThread>
+        cpu = cpursc.allocate_whole_core()
+        spec= "s#<xRANThread>.*,.*,.*</xRANThread>#<xRANThread>CPU_NUM, 6, 0</xRANThread>#"  
+        result = re.sub("CPU_NUM", str(cpu), spec)
+        subprocess.call(["sed", "-i", result, oru_cfgfile])
+        
+        # step 4: Alloc a CPU for xRANWorker, but fixup in cpu_mask
+        # <xRANWorker>0x80000, 96, 0</xRANWorker>
+        core_mask = cpursc.allocate_siblings_mask(1)
+        logger.debug("mask=" + str(core_mask))
+        spec= "s#<xRANWorker>.*,.*,.*</xRANWorker>#<xRANWorker>CPU_MASK, 6, 0</xRANWorker>#"  
+        result = re.sub("CPU_MASK", str(core_mask), spec)
+        subprocess.call(["sed", "-i", result, oru_cfgfile])
+
+    @classmethod
+    def update_cfg_files(cls, l1_cfg_file: str, testmac_cfg_file: str, cpursc: CpuResource):
+        print ("Updating %s %s:" % (l1_cfg_file, testmac_cfg_file));
+        
         # First in list is the HK_CPUS, Use whole core for systemThreads abd RunThread
         cpu = cpursc.allocate_whole_core()
         spec= "s#<systemThread>.*,.*,.*</systemThread>#<systemThread>CPU_NUM, 0, 0</systemThread>#"  
@@ -51,43 +86,64 @@ class Setting:
         result = re.sub("CPU_NUM", str(cpu), spec)
         subprocess.call(["sed", "-i", result, testmac_cfg_file])
 
-
     # Call the update_tesfile function of the ProcessTestfile class (in
     # process_testfile.py).
     @classmethod
     def update_testfile(cls, rsc, testfile, phystart_quick=False):
         ProcessTestfile.update_testfile(rsc, testfile, phystart_quick)
 
+import argparse
 def main(name, argv):
-    logger.info("Enter: ")
     nosibling = False
     nohkcpus = False
-    phystart = False
+    phystart = None
     testfile = None
-    helpstr = name + " --testfile=<testfile path>"\
-                     " --nosibling"\
-                     " --nohkcpus"\
-                     " --phystart">\
-                     "\n"
-    try:
-        opts, args = getopt.getopt(argv,"h",["testfile=", "nosibling", "phystart", "nohkcpus"])
-    except getopt.GetoptError:
-        logger.info("error")
-        print(helpstr)
-        sys.exit(2)
+    l1_cfgfile = None
+    testmac_cfgfile = None
+    oru_cfgfile = None
+    vfs_pci = []
+    xran = False
+    print (sys.argv[1:]);
 
-    for opt, arg in opts:
-        if opt == '-h':
-            print(helpstr)
-            sys.exit()
-        elif opt in ("--testfile"):
-            testfile = arg
-        elif opt in ("--nosibling"):
-            nosibling = True
-        elif opt in ("--phystart"):
-            phystart = True
-        elif opt in ("--nohkcpus"):
-            nohkcpus = True
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--testfile', type=str, required=False)
+    parser.add_argument('--l1_cfgfile', type=str, required=False)
+    parser.add_argument('--testmac_cfgfile', type=str, required=False)
+    parser.add_argument('--oru_cfgfile', type=str, required=False)
+    parser.add_argument('--oru_vfs_pci', type=str, required=False)
+    # https://intellipaat.com/community/4618/argparse-module-how-to-add-option-without-any-argument
+    parser.add_argument('--nosibling', action='store_true')
+    parser.add_argument('--phystart', action='store_true')
+    parser.add_argument('--nohkcpus', action='store_true')
+    parser.add_argument('--xran', action='store_true')
+
+    arg = parser.parse_args()
+    if arg.testfile:
+        testfile = arg.testfile
+        logger.debug("--testfile= " + str(testfile))
+    if arg.l1_cfgfile:
+        l1_cfgfile = arg.l1_cfgfile
+        logger.debug("--l1_cfgfile= " + str(l1_cfgfile))
+    if arg.testmac_cfgfile:
+        testmac_cfgfile = arg.testmac_cfgfile
+        logger.debug("--testmac_cfgfile= " + str(testmac_cfgfile))
+    if arg.oru_cfgfile:
+        oru_cfgfile = arg.oru_cfgfile
+        logger.debug("--oru_cfgfile= " + oru_cfgfile)
+    if arg.oru_vfs_pci:
+        vfs_pci = arg.oru_vfs_pci
+        logger.debug("--oru_vfs_pci= " + str(vfs_pci))
+    if arg.nosibling:
+        nosibling = True
+        logger.debug("--nosibling")
+    if arg.phystart:
+        phystart = True
+        logger.debug("--phystart= " + str(phystart))
+    if arg.nohkcpus:
+        nohkcpus = True
+    if arg.xran:
+        xran = True
+
     # Note - Crucible does not use '/proc/self/status'
     #   status_content = open(procstatus).read().rstrip('\n')
     #   cpursc = CpuResource(status_content, nosibling)
@@ -105,13 +161,22 @@ def main(name, argv):
 
     # Note: update_cfg_files must always be called in order to remove any common cpus
     # from cpursc before cpursc is used by update_testfile below.
-    Setting.update_cfg_files(None, cpursc)
+
+    Setting.update_cfg_files(l1_cfgfile, testmac_cfgfile, cpursc)
     logger.debug("after cfg_files, cpus available: " + str(cpursc.available))
+
+    # ORU config
+    if oru_cfgfile is not None:
+        logger.debug("HN pre oru_cfg=" + str(oru_cfgfile))
+        logger.debug("HN pre vfs_pci=" + str(vfs_pci))
+        Setting.update_xran_cfg_files(oru_cfgfile, vfs_pci, cpursc)
+            
 
     if testfile is not None:
         # NOTE: QUICK PHYSTART NOTED BY TRUE, CHANGE FOR REAL TESTS.
         Setting.update_testfile(cpursc, testfile, phystart)
         print('Test file updated: %s' % testfile)
+
 
 if __name__ == "__main__":
      main(sys.argv[0], sys.argv[1:])
