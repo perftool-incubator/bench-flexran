c3547c4835b56c26c08dcb90a542fb8a82348ccd
HughNhan <hnhan@redhat.com>
2022-05-04 17:23:00 -0400

    incorporate workload-partition CPU to performanceprofile

--- script-dir/03-performance_operator_cleanup.sh
+++ script-dir/03-performance_operator_cleanup.sh
@@ -10,6 +10,10 @@ parse_args $@
 echo "Removing performance profile ..."
 [ -e ${MANIFEST_DIR}/performance_profile.yaml ] && oc delete -f ${MANIFEST_DIR}/performance_profile.yaml
 
+if [[ "${SNO}" == "true" ]]; then
+   oc label mcp master machineconfiguration.openshift.io/role-
+fi
+
 if [[ "${WAIT_MCP}" == "true" ]]; then
     wait_mcp
 fi
--- script-dir/03-performance_operator_install.sh
+++ script-dir/03-performance_operator_install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Install PAO and performanceprofile for SNO or reg cluster
-# Usage:
-#   export SNO=true  - For for SNO
-#   export NSO-false - For regular cluster
+# Install PAO and performanceprofile for SNO or regular cluster
 #
+# Note::
+#    - for SNO: incorporate the workload-partitioning CPU list into isolated and reserved computations.
+#    - for non-SNO: hardcode 2 CPUs for mgmt workloads.
 
 set -euo pipefail
 
@@ -13,11 +13,7 @@ source ./functions.sh
 
 parse_args $@
 
-# registry is not relevent to the performance operator
-# but let's take care of this as early as possible
-#<HN>set_registry
-
-if [[ "${SNO}" == "false" ]]; then
+if [ "${SNO}" == "false" ]; then
   echo oc label --overwrite node ${BAREMETAL_WORKER} node-role.kubernetes.io/worker-cnf=""
 fi
 
@@ -38,7 +34,19 @@ wait_pod_in_namespace openshift-performance-addon
 ###### generate performance profile ######
 echo "Acquiring cpu info from worker node ${BAREMETAL_WORKER} ..."
 all_cpus=$(exec_over_ssh ${BAREMETAL_WORKER} lscpu | awk '/On-line CPU/{print $NF;}')
-export DU_RESERVED_CPUS=$(exec_over_ssh ${BAREMETAL_WORKER} "cat /sys/bus/cpu/devices/cpu0/topology/thread_siblings_list")
+
+if [ "${SNO}" == "false" ]; then
+    export DU_RESERVED_CPUS=$(exec_over_ssh ${BAREMETAL_WORKER} "cat /sys/bus/cpu/devices/cpu0/topology/thread_siblings_list")
+else
+    file="/etc/kubernetes/openshift-workload-pinning"
+    if [ "$(exec_over_ssh ${BAREMETAL_WORKER} "test -e $file && echo true")" == "true" ]; then
+        # awk: match "cpuset" line, strip double quotes from NF , print NF 
+        export DU_RESERVED_CPUS=$(exec_over_ssh ${BAREMETAL_WORKER} "cat /etc/kubernetes/openshift-workload-pinning" | awk '/cpuset/{gsub(/"/, "", $NF); print $NF;}' )
+    else
+        echo "WARNING this SNO has no workload-paritioning. Please fix it and then try again"
+        exit;
+    fi
+fi
 
 PYTHON=$(get_python_exec)
 export DU_ISOLATED_CPUS=$(${PYTHON} cpu_cmd.py cpuset-substract ${all_cpus} ${DU_RESERVED_CPUS})
@@ -54,12 +62,11 @@ envsubst < templates/performance_profile.yaml.template > ${MANIFEST_DIR}/perform
 echo "generating ${MANIFEST_DIR}/performance_profile.yaml: done"
 
 ##### apply performance profile ######
-if [[ "${SNO}" == "true" ]]; then
+if [ "${SNO}" == "true" ]; then
    oc label mcp master machineconfiguration.openshift.io/role=master
 else
    ./create_mcp.sh
 fi
-
 
 echo "apply ${MANIFEST_DIR}/performance_profile.yaml ..."
 oc apply -f ${MANIFEST_DIR}/performance_profile.yaml
